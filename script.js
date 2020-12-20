6b1f1424a68269ca25079c258db24166f75c68f0
tmannazy <e.anene@hotmail.com>
2020-12-20 16:36:04 +0100

    Center the container that keep the grids

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
 // Create a reset button for the game
 // When user clicks on the button, clear the colored boxes
 // Ask user to enter the number of boxes to create dynamically
-// If the number is greater than 100, ask user to enter a lesser number
+// If the number is greater than 50, ask user to enter a lesser number
 // Display selected boxes
 // Move over the boxes to make a sketch as it colors the board
 // Make your desired sketch.
@@ -12,35 +12,45 @@
 
 
 
-// const container = document.querySelector('.container');
+const container = document.querySelector('.container');
+const button = document.querySelector('#button');
+const gridCells = document.querySelectorAll('.grid-cells');
 
-// function createGrid(rows, cols) {
-//     container.style.setProperty('--grid-rows', rows);
-//     container.style.setProperty('--grid-cols', cols);
-//     for (let i = 0; i < (rows * cols); i++){
-//         let cell = document.createElement('div');
-//         cell.style.width = rows + 'px';
-//         cell.style.height = cols + 'px';
-//         container.appendChild(cell).className = 'grid-cells';
-//     }
-// }
+function createGrid(rows, cols) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    for (let i = 0; i < (rows * cols); i++){
+        let cell = document.createElement('div');
+        container.appendChild(cell).className = 'grid-cells';
+    }
+}
 
-// createGrid(8, 8);
 
-// const gridCells = document.querySelectorAll('.grid-cells');
+// function resetGrid() {
+//         // createGrid(5, 5);
+//         document.querySelectorAll('.grid-cells').forEach(item => item.style = '');
+// };
 
-// const button = document.querySelector('#button');
+// button.addEventListener('click', resetGrid);
+button.addEventListener('click', () => {
+    let gridNum = parseInt(prompt('Enter your grid size', 'Type a number'));
+    if (/^[0-9.,]+$/.test(gridNum) && gridNum > 1 && gridNum <= 50) {
+        createGrid(gridNum, gridNum);
+        // resetGrid();
+    } else {
+        alert('Enter a grid size between 1 and  50');
+    }
+});
 
-// button.addEventListener('click', () => {
-//     let gridNum = parseInt(prompt('Enter your grid length', 'Type a number'));
-//     if (/^[0-9.,]+$/.test(gridNum) && gridNum > 1 && gridNum <= 100) {
-//         createGrid(gridNum, gridNum);
-//     } else {
-//         alert('Enter a length of 1 and less than 100');
-//     }
-// });
+gridCells.forEach(cells => {
+    cells.addEventListener('mouseover', () => cells.style.backgroundColor = 'red');
+});
 
-// gridCells.forEach(cells => {
-//     cells.addEventListener('mouseover', () => cells.style.backgroundColor = 'red');
-// });
+
+function gridDisplay() {
+    createGrid(5, 5);
+    // createGrid(gridNum, gridNum);
+}
+
+gridDisplay();
 
